4ba63b48aae28f502501d30e5e14f8af01f53931
Marvin Li <marvinli@college.harvard.edu>
2024-12-10 16:44:58 -0500

    working on jailbreak practical experiments

--- experiments/jailbreak/run_likelihood_ratio_jailbreak.py
+++ experiments/jailbreak/run_likelihood_ratio_jailbreak.py
@@ -30,26 +30,53 @@ python experiments/jailbreak/run_likelihood_ratio_jailbreak.py --aligned_model m
     --unaligned_model grimjim/Llama-3.1-8B-Instruct-abliterated_via_adapter   \
     --num_samples 10 --num_repeats 10
 """
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 def run_evaluation(generator, model, dataset, dataset_with_prefill, prefix, experiment_dir):
     # Generate logprobs
     print(f"Generating logprobs for {prefix} dataset")
     logprobs = generator.generate_responses(dataset_with_prefill, model.sampling_logprobs, return_logprobs=True)
     
-    # Generate responses
-    print(f"Generating responses for {prefix} dataset")
-    responses = generator.generate_responses(dataset, model.sampling_repeat)
-    
-    del model.model
-    torch.cuda.empty_cache()
-
-    # Grade responses
-    print(f"Grading responses for {prefix} dataset")
-    graded = generator.grade(responses)
-    
     # Save results
     torch.save(logprobs, f"{experiment_dir}/{prefix}_logprobs.pt")
-    torch.save(graded, f"{experiment_dir}/{prefix}_graded.pt")
+    
+    if dataset:
+        # Generate responses
+        print(f"Generating responses for {prefix} dataset")
+        responses = generator.generate_responses(dataset, model.sampling_repeat)
+        
+        del model.model
+        torch.cuda.empty_cache()
+
+        # Grade responses
+        print(f"Grading responses for {prefix} dataset")
+        graded = generator.grade(responses)
+        
+        # Save results
+        torch.save(graded, f"{experiment_dir}/{prefix}_graded.pt")
+
+
+def load_model_and_generator(args, aligned):
+    if aligned:
+        model = load_all(model_id=args.aligned_model,
+                                max_gen_length=args.max_gen_length,
+                                seed=args.seed,
+                                num_per_noise=args.num_repeats,
+                                log_probs=True)
+    else:
+        model = load_all(
+            model_id=args.unaligned_model,
+            generation_config_id=args.aligned_model,
+            max_gen_length=args.max_gen_length,
+            seed=args.seed,
+            num_per_noise=args.num_repeats,
+            log_probs=True
+        )
+    generator = GenerateEvalJailbreak(
+        genwrapper=model,
+        tokenizer=model.tokenizer
+    )
+    return model, generator 
 
 def main():
     ####################################################################################################
@@ -67,6 +94,7 @@ def main():
     
     ## Dataset argument
     parser.add_argument('--jailbreak_dataset', action="store", type=str, required=False, default=JAILBREAK_DATASET, help='Type of jailbreak dataset')
+    parser.add_argument('--regular_dataset', action="store", type=str, required=False, default=REGULAR_DATASET, help='Type of regular dataset')
     parser.add_argument('--num_samples', action="store", type=int, required=False, default=None, help='Number of samples.')
     parser.add_argument('--num_repeats', action="store", type=int, required=False, default=1, help='Number of repeats to evaluate jailbreaks.')
     
@@ -91,11 +119,7 @@ def main():
     ############################################################################################################################################################################################################################################
     set_seed(args.seed)
     
-    aligned_model = load_all(model_id=args.aligned_model,
-                             max_gen_length=args.max_gen_length,
-                             seed=args.seed,
-                             num_per_noise=args.num_repeats,
-                             log_probs=True)
+    aligned_model, aligned_generator = load_model_and_generator(args, aligned=True)
     jailbreak_dataset = create_prefill_dataset(
         tokenizer=aligned_model.tokenizer,
         dataset=args.jailbreak_dataset,
@@ -104,17 +128,25 @@ def main():
         filter_orig=False,
         jailbreak_suffix=""
     )
-
     # Add evaluation logprob string to the dataset
     jailbreak_dataset_with_prefill = copy.deepcopy(jailbreak_dataset)
     jailbreak_dataset_with_prefill = jailbreak_dataset_with_prefill.map(
         lambda example: {**example, "context": example["context"] + args.evaluate_logprob_str}
     )
 
-    aligned_generator = GenerateEvalJailbreak(
-        genwrapper=aligned_model,
-        tokenizer=aligned_model.tokenizer
+    regular_dataset =  create_prefill_dataset(
+        tokenizer=aligned_model.tokenizer,
+        dataset=args.regular_dataset,
+        char_step=None,
+        num_samples=args.num_samples,
+        filter_orig=False,
+        jailbreak_suffix=""
     )
+    
+    regular_dataset = regular_dataset.map(
+        lambda example: {**example, "context": example["context"] + args.evaluate_logprob_str}
+    )
+    
     ################################################################################################################################################################
     ## Run evaluation for aligned model
     ################################################################################################################################################################
@@ -127,24 +159,22 @@ def main():
         prefix="aligned_jailbreak",
         experiment_dir=args.experiment_dir
     )
+    aligned_model, aligned_generator = load_model_and_generator(args, aligned=True)
 
+    run_evaluation(
+        generator=aligned_generator,
+        model=aligned_model,
+        dataset=None,
+        dataset_with_prefill=regular_dataset,
+        prefix="aligned_benign",
+        experiment_dir=args.experiment_dir
+    )
+    del aligned_model.model
+    
     ################################################################################################################################################################
     ## Run evaluation for unaligned model
     ################################################################################################################################################################
-    unaligned_model = load_all(
-        model_id=args.unaligned_model,
-        generation_config_id=args.aligned_model,
-        max_gen_length=args.max_gen_length,
-        seed=args.seed,
-        num_per_noise=args.num_repeats,
-        log_probs=True
-    )
-
-    unaligned_generator = GenerateEvalJailbreak(
-        genwrapper=unaligned_model, 
-        tokenizer=unaligned_model.tokenizer
-    )
-
+    unaligned_model, unaligned_generator = load_model_and_generator(args, aligned=False)
     print("Running evaluation for unaligned model")
     run_evaluation(
         generator=unaligned_generator,
@@ -154,10 +184,17 @@ def main():
         prefix="unaligned_jailbreak",
         experiment_dir=args.experiment_dir
     )
-
+    
+    unaligned_model, unaligned_generator = load_model_and_generator(args, aligned=False)
+    run_evaluation(
+        generator=unaligned_generator,
+        model=aligned_model,
+        dataset=None,
+        dataset_with_prefill=regular_dataset,
+        prefix="unaligned_benign",
+        experiment_dir=args.experiment_dir
+    )
     print(f"Experiment completed in {time.time() - start_time:.2f} seconds.")
-
-
 
 
 if __name__ == "__main__":
